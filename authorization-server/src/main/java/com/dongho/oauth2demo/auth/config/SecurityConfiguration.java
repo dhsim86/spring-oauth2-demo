f69dd6d8e91537e2178542205b4fb4ce8a571999
심동호 <dhsim86@gmail.com>
2022-12-23 16:28:14 +0900

    init oauth2 demo

---
+++ authorization-server/src/main/java/com/dongho/oauth2demo/auth/config/SecurityConfiguration.java
@@ -0,0 +1,35 @@
+package com.dongho.oauth2demo.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+	@Bean
+	public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeRequests(authorizeRequests ->
+			authorizeRequests.anyRequest().authenticated()
+		)
+			.formLogin(Customizer.withDefaults());
+		return http.build();
+	}
+
+	@Bean
+	public UserDetailsService users() {
+		UserDetails user = User.withDefaultPasswordEncoder()
+			.username("dongho")
+			.password("1234")
+			.roles("USER")
+			.build();
+		return new InMemoryUserDetailsManager(user);
+	}
+
+}
---
+++ client/src/main/java/com/dongho/oauth2demo/client/presentation/web/ArticlesController.java
@@ -0,0 +1,31 @@
+package com.dongho.oauth2demo.client.presentation.web;
+
+import static org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction.*;
+
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+public class ArticlesController {
+
+	private final WebClient webClient;
+
+	@GetMapping(value = "/articles")
+	public String[] getArticles(
+		@RegisteredOAuth2AuthorizedClient("articles-client-authorization-code") OAuth2AuthorizedClient authorizedClient
+	) {
+		return this.webClient
+			.get()
+			.uri("http://127.0.0.1:8082/articles")
+			.attributes(oauth2AuthorizedClient(authorizedClient))
+			.retrieve()
+			.bodyToMono(String[].class)
+			.block();
+	}
+}

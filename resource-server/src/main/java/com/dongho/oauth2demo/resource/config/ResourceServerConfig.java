1c9596266450d2e5cfd5efef2328c2eef6274a3e
심동호 <dhsim86@gmail.com>
2023-01-25 17:01:49 +0900

    scope 수정

--- resource-server/src/main/java/com/dongho/oauth2demo/resource/config/ResourceServerConfig.java
+++ resource-server/src/main/java/com/dongho/oauth2demo/resource/config/ResourceServerConfig.java
@@ -13,7 +13,7 @@ public class ResourceServerConfig {
 		http.mvcMatcher("/articles/**")
 			.authorizeRequests()
 			.mvcMatchers("/articles/**")
-			.access("hasAuthority('SCOPE_news.donga')")
+			.access("hasAuthority('SCOPE_test')")
 			.and()
 			.oauth2ResourceServer()
 			.jwt();
